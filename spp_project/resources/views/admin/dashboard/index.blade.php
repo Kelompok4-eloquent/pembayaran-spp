751a45e5cb248cfdb0fb7fd8c410f9bc54da4690
dzaki236 <dzakiahnafz@gmail.com>
2021-05-05 21:01:56 +0700

    menambahkan count dashboard data (dummy)

--- spp_project/resources/views/admin/dashboard/index.blade.php
+++ spp_project/resources/views/admin/dashboard/index.blade.php
@@ -1,63 +1,32 @@
 @extends('admin.partials.main.master')
 @section('title')
-    Dashboard
+Dashboard
 @endsection
 @section('content')
 <div class="row">
     <div class="col-lg-4 col-md-4 col-sm-12">
-      <div class="card card-statistic-2">
-        <div class="card-chart">
-          <canvas id="balance-chart" height="80"></canvas>
+        <div class="card card-statistic-2">
+            <div class="card-body pt-5 pb-5 pl-5">
+                <h1 class="display-4">1200</h1>
+                <a href="#" class="text-muted h5 pl-1">Data Siswa > </a>
+            </div>
         </div>
-        <div class="card-icon shadow-primary bg-primary">
-          <i class="fas fa-dollar-sign"></i>
-        </div>
-        <div class="card-wrap">
-          <div class="card-header">
-            <h4>Balance</h4>
-          </div>
-          <div class="card-body">
-            $187,13
-          </div>
-        </div>
-      </div>
     </div>
     <div class="col-lg-4 col-md-4 col-sm-12">
         <div class="card card-statistic-2">
-          <div class="card-chart">
-            <canvas id="balance-chart" height="80"></canvas>
-          </div>
-          <div class="card-icon shadow-primary bg-primary">
-            <i class="fas fa-dollar-sign"></i>
-          </div>
-          <div class="card-wrap">
-            <div class="card-header">
-              <h4>Balance</h4>
+            <div class="card-body pt-5 pb-5 pl-5">
+                <h1 class="display-4">2</h1>
+                <a href="#" class="text-muted h5 pl-1">Petugas & administrator > </a>
             </div>
-            <div class="card-body">
-              $187,13
-            </div>
-          </div>
         </div>
-      </div>
-    <div class="col-lg-4 col-md-4 col-sm-12">
-      <div class="card card-statistic-2">
-        <div class="card-chart">
-          <canvas id="sales-chart" height="80"></canvas>
-        </div>
-        <div class="card-icon shadow-primary bg-primary">
-          <i class="fas fa-shopping-bag"></i>
-        </div>
-        <div class="card-wrap">
-          <div class="card-header">
-            <h4>Sales</h4>
-          </div>
-          <div class="card-body">
-            4,732
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
+    <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card card-statistic-2">
+            <div class="card-body pt-5 pb-5 pl-5">
+                <h1 class="display-4">12</h1>
+                <a href="#" class="text-muted h5 pl-1">Kelas ></a>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
