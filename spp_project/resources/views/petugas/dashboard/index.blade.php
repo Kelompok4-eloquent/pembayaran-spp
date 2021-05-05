f5ef07e81ab8b5d9fb4a24f37d60026cd79342dc
dzaki236 <dzakiahnafz@gmail.com>
2021-05-05 21:19:48 +0700

    menambahkan semua data dummy pada halaman petugas maupun admin

--- spp_project/resources/views/petugas/dashboard/index.blade.php
+++ spp_project/resources/views/petugas/dashboard/index.blade.php
@@ -1,7 +1,18 @@
 @extends('admin.partials.main.master')
-@section('title')
-    Dashboard
+@section('title') 
+Dashboard Petugas
 @endsection
 @section('content')
-    
-@endsection
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="card card-statistic-2">
+            <div class="card-body pt-5 pb-5 pl-5 row">
+                <div class="col-10">
+                    <h1 class="display-4">1100</h1>
+                    <p class="text-muted h5 pl-1">Transaksi lunas</p>
+                </div>
+                <div class="col-2"><a href="#" class="text-muted h6 ">Tambah Transaksi [+]</a></div>
+            </div>
+        </div>
+    </div>
+</div>@endsection
--- spp_project/resources/views/petugas/partials/main/master.blade.php
+++ spp_project/resources/views/petugas/partials/main/master.blade.php
@@ -12,8 +12,8 @@
   <!-- CSS Libraries -->
 
   <!-- Template CSS -->
-  <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
 </head>
 
 <body>
@@ -21,18 +21,16 @@
     <div class="main-wrapper">
       @include('admin.partials.layouts.navbar')
       @include('admin.partials.layouts.sidebar')
-     <!-- Main Content -->
-     <div class="main-content">
-      <section class="section">
-        <div class="section-header">
-          <h1>@yield('title')</h1>
-        </div>
 
-        <div class="section-body">
-          @yield('content')
-        </div>
-      </section>
-    </div>
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>@yield('title')</h1>
+          </div>
+            @yield('content')
+        </section>
+      </div>
      @include('admin.partials.layouts.footer')
     </div>
   </div>
@@ -43,14 +41,16 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="{{asset('assets/js/stisla.js')}}"></script>
+  <script src="{{ asset('assets/js/stisla.js') }}"></script>
 
   <!-- JS Libraies -->
 
   <!-- Template JS File -->
-  <script src="{{asset('assets/js/scripts.js')}}"></script>
-  <script src="{{asset('assets/js/custom.js')}}"></script>
+  <script src="{{ asset('assets/js/scripts.js') }}"></script>
+  <script src="{{ asset('assets/js/custom.js') }}"></script>
 
   <!-- Page Specific JS File -->
+  <script src="{{ asset('assets/js/page/index.js') }}"></script>
+  <script src="{{ asset('assets/js/page/modules-chartjs.js') }}"></script>
 </body>
 </html>
