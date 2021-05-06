cc20bc62fae69eb3564b452bb785783d2dc901a6
dzaki236 <dzakiahnafz@gmail.com>
2021-05-06 22:29:47 +0700

    add active style to css (menambahkan style active)

--- spp_project/resources/views/admin/partials/main/master.blade.php
+++ spp_project/resources/views/admin/partials/main/master.blade.php
@@ -8,36 +8,6 @@
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-<style>
-  .active-menu{
-    padding-left: 16px;
-    position: relative;
-    
-}
-.active-menu a {
-  color: #6777ef !important;
-    font-weight: 600;
-}
-.active-menu:before {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 50%;
-    transform: translateY(-50%);
-    height: 25px;
-    width: 4px;
-    background-color: #6777ef;
-}
-
-/* .active-parent{
-  color:#6777ef;
-  font-weight: 600;
-    background-color: #f8fafb;
-} */
-.active-link a{
-  color:#6777ef !important;
-}
-</style>
   <!-- CSS Libraries -->
 
   <!-- Template CSS -->
