dbb3df757012b50575f40e18b65f3080e47b4b6e
dzaki236 <dzakiahnafz@gmail.com>
2021-05-06 23:16:38 +0700

    membuat dan menambahkan halaman data_siswa

--- spp_project/resources/views/admin/data_siswa/index.blade.php
+++ spp_project/resources/views/admin/data_siswa/index.blade.php
@@ -6,10 +6,45 @@ Dashboard
 <div class="section-header">
     <h1>Data Siswa</h1>
 </div>
-
-{{-- <div class="section-header-breadcrumb">
-    <div class="breadcrumb-item active"><a href="#">Data Siswa</a></div>
-    <div class="breadcrumb-item"><a href="#">Forms</a></div>
-    <div class="breadcrumb-item">Advanced Forms</div>
-</div> --}}
+<a href="{{ url('admin/data_siswa/tambah_siswa') }}" class="btn btn-success mb-4">[+] Tambah Data Siswa</a>
+<div class="row">
+    <div class="col-12 col-md-6 col-lg-12 col-xl-12">
+        <div class="card">
+            <div class="card-header">
+                <h2>Data Siswa</h2>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-md">
+                        <tbody>
+                            <tr>
+                                <th>No</th>
+                                <th>NISN</th>
+                                <th>NIS</th>
+                                <th>Nama</th>
+                                <th>Kelas</th>
+                                <th>Alamat</th>
+                                <th>No Telp</th>
+                                <th>Tahun Masuk</th>
+                                <th>Action</th>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>012345</td>
+                                <td>0123455566</td>
+                                <td>Abdul</td>
+                                <td>XI RPL 2</td>
+                                <td>Jalan Abc</td>
+                                <td>08123456789</td>
+                                <td>2020</td>
+                                <td><a href="" class="m-2 btn btn-warning">Edit</a><a href=""
+                                        class="btn btn-danger m-2">Delete</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- spp_project/resources/views/admin/data_siswa/tambah_siswa.blade.php
+++ spp_project/resources/views/admin/data_siswa/tambah_siswa.blade.php
@@ -11,4 +11,85 @@ Dashboard
     <div class="breadcrumb-item active"><a href="#">Data Siswa</a></div>
     <div class="breadcrumb-item">Tambah Data Siswa</div>
 </div>
+
+<div class="row">
+    <div class="ol-12 col-md-6 col-lg-12 col-xl-12">
+        <div class="card">
+            <div class="card-header">
+                <h3>Tambah Data Siswa</h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12 col-md-12 col-lg-12 col-xl-12">
+                         {{-- input type text (typenya aja di pilih) --}}
+                        <div class="form-group">
+                            <label>NISN : </label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-12 col-md-12 col-lg-12 col-xl-12">
+                        <div class="form-group">
+                            <label>NIS : </label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                   
+                    <br>
+                    <div class="col-12 col-md-6 col-lg-6 col-xl-6">
+                        <div class="form-group">
+                            <label>Nama Depan : </label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-6 col-xl-6">
+                        <div class="form-group">
+                            <label>Nama Belakang : </label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-12 col-md-12 col-lg-12 col-xl-12">
+                        {{-- input type number (typenya aja di pilih) --}}
+                        <div class="form-group">
+                            
+                            <label>No Telp : </label>
+                            <input type="number" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-12 col-lg-12 col-xl-12">
+                        {{-- textarea --}}
+                        <div class="form-group">
+                            <label>Alamat : </label>
+                            <textarea class="form-control" cols="30" rows="50"></textarea>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-12 col-md-6 col-lg-6 col-xl-6">
+                        <div class="form-group">
+                            <label>Kelas : </label>
+                            <select class="form-control">
+                                <option disabled selected>== Pilih Kelas ==</option>
+                                <option>Option 1</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-6 col-xl-6">
+                       {{-- Select Option --}}
+                        <div class="form-group">
+                            <label>Tahun Masuk : </label>
+                            <select class="form-control">
+                                <option disabled selected>== Pilih Tahun Masuk ==</option>
+                                <option>Option 1</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <button class="btn btn-primary btn-block m-3">Simpan</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
 @endsection
