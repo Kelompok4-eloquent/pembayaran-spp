86d2aa2c644e266e660105eb5aa87dd3d269ef4f
dzaki236 <dzakiahnafz@gmail.com>
2021-05-08 22:18:02 +0700

    add model (memperbaiki logic pada model)

---
+++ spp_project/app/Models/Pembayaran.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pembayaran extends Model
+{
+    use HasFactory;
+    protected $table='pembayaran';
+    protected $fillable = ['id_pembayaran'];
+    // public function pembayaran()
+    // {
+    //     # Pembayaran
+    //     return $this->belongsTo(Pembayaran::class,'id_pembayaran','id_pembayaran');
+    //     // return $this->hasOne(Spp::class,'id_spp','id_spp');
+    // }
+    protected $with = ['siswa','petugas'];
+    // protected $appends = ['nama_kelas'];
+    public function siswa(){
+        # Siswa
+        return $this->belongsTo(Siswa::class,'nisn','nisn');
+    }
+    // public function kelass()
+    // {
+    //     # Kelas
+    //     return $this->hasManyThrough(Kelas::class,Siswa::class,'id_kelas','id_kelas');
+    // }
+    public function petugas()
+    {
+        # code...
+        return $this->belongsTo(Petugas::class,'id_petugas','id_petugas');
+    }
+    public function spp_siswa()
+    {
+        # code...
+        return $this->belongsTo(Spp::class,'id_spp','id_spp');
+    }
+    // public function getNamaKelasAttribute()
+    // {
+    //     # code...
+    //     return $this->siswa->kelas->nama_kelas;
+    // }
+}
