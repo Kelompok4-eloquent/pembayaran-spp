7b1dd7b7f30ef016cd8f1a744ba951fb395ea45b
dzaki236 <dzakiahnafz@gmail.com>
2021-05-10 00:02:35 +0700

    fixing bug di bagian login (di awalnya)

--- spp_project/config/auth.php
+++ spp_project/config/auth.php
@@ -68,7 +68,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Petugas::class,
+            'table' => 'petugas'
         ],
 
         // 'users' => [
